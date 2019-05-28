cd3c8efdbd0ed0b848020ef81f505773c6304a1f
Michael Klementschitz <23321801+MichaelKlementschitz@users.noreply.github.com>
2019-05-28 08:49:59 +0200

    Added setText

--- ButtonMonitor/buttonmonitor.cpp
+++ ButtonMonitor/buttonmonitor.cpp
@@ -20,6 +20,7 @@ void ButtonMonitor::updateValue()
 {
     for(int i = 1; i <= 3; i++)
     {
+        (findChild<QLabel *>("btn" + QString::number(i)))->setText(QString::number(1-digitalRead(Butn[i-1])));
         (findChild<QLabel *>("btn" + QString::number(i)))->setStyleSheet((1-digitalRead(Butn[i-1])) ? "background-color:red;color: black;" : "background-color:black;color: white;");
     }
 }
